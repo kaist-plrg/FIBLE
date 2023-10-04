6b55d5b402ee7332f2bfd4875b53d40e34521da0
jihee Park <jiheepark@kaist.ac.kr>
2023-10-04 15:31:26 +0900

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# check if opam local switch already exists
+if [ ! -d "_opam" ]; then
+  opam switch -y create . --deps-only --packages=ocaml-base-compiler.4.14.1
+else
+  dune build
+  opam install --deps-only .
+fi
