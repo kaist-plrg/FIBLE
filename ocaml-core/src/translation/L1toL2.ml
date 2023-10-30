58a156f225df89b8a01484f2e898c0e597601567
jihee Park <jiheepark@kaist.ac.kr>
2023-10-30 15:11:09 +0900

    Translate sload/sstore

--- ocaml-core/src/translation/L1toL2.ml
+++ ocaml-core/src/translation/L1toL2.ml
@@ -1,5 +1,5 @@
-let translate_jmp (j : L1.Jmp.t_full) (a : L1.SPFA.Immutable.t) : L2.Jmp.t_full
-    =
+let translate_jmp (j : L1.Jmp.t_full) (ga : L1.SPFA.Immutable.t)
+    (la : L1.AbsState.t) : L2.Jmp.t_full =
   let njmp : L2.Jmp.t =
     match j.jmp with
     | Junimplemented -> Junimplemented
@@ -14,23 +14,44 @@ let translate_jmp (j : L1.Jmp.t_full) (a : L1.SPFA.Immutable.t) : L2.Jmp.t_full
 
   { jmp = njmp; loc = j.loc; mnem = j.mnem }
 
-let translate_inst (i : L1.Inst.t_full) (a : L1.SPFA.Immutable.t) :
-    L2.Inst.t_full =
+let translate_inst (i : L1.Inst.t_full) (ga : L1.SPFA.Immutable.t)
+    (la : L1.AbsState.t) : L2.Inst.t_full =
   let nins : L2.Inst.t =
     match i.ins with
     | INop -> INop
     | Iassignment (d, s) -> Iassignment (d, s)
-    | Iload (d, s, o) -> Iload (d, s, o)
-    | Istore (d, o, s) -> Istore (d, o, s)
+    | Iload (d, s, o) -> (
+        match s with
+        | Register r -> (
+            match L1.AbsState.find_opt r.id la with
+            | Some { have_sp = Flat true; offset = Flat c } ->
+                Isload ({ value = c; width = 8l }, o)
+            | _ -> Iload (d, s, o))
+        | _ -> Iload (d, s, o))
+    | Istore (d, o, s) -> (
+        match s with
+        | Register r -> (
+            match L1.AbsState.find_opt r.id la with
+            | Some { have_sp = Flat true; offset = Flat c } ->
+                Isstore ({ value = c; width = 8l }, o)
+            | _ -> Istore (d, o, s))
+        | _ -> Istore (d, o, s))
   in
   { ins = nins; loc = i.loc; mnem = i.mnem }
 
-let translate_block (b : L1.Block.t) (a : L1.SPFA.Immutable.t) : L2.Block.t =
-  {
-    loc = b.loc;
-    body = List.map (fun i -> translate_inst i a) b.body;
-    jmp = translate_jmp b.jmp a;
-  }
+let translate_block (b : L1.Block.t) (ga : L1.SPFA.Immutable.t) : L2.Block.t =
+  let astate = L1.FSAbsD.AbsLocMapD.find_opt b.loc ga.states.pre_state in
+  let body, final_a =
+    match astate with
+    | Some v ->
+        List.fold_left
+          (fun (acci, a) i -> (acci @ [ translate_inst i ga a ], a))
+          ([], v) b.body
+    | None ->
+        ( List.map (fun i -> translate_inst i ga L1.AbsState.top) b.body,
+          L1.AbsState.top )
+  in
+  { loc = b.loc; body; jmp = translate_jmp b.jmp ga final_a }
 
 let translate_func (f : L1.Func.t) (a : L1.SPFA.Immutable.t) : L2.Func.t =
   {
