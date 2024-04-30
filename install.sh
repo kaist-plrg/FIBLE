4574e141708b76851fb1bc3b7c0d976318c798b1
Jihee Park <j31d0@users.noreply.github.com>
2024-04-30 16:29:44 +0900

    test restructure, fixing install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Check if opam is installed
+if command -v opam &> /dev/null
+then
+    echo "opam is installed."
+else
+    echo "opam is not installed."
+    echo "To install opam, please run:"
+    echo "sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)"
+    exit 1
+fi
+
 # check if opam local switch already exists
 if [ ! -d "_opam" ]; then
   opam switch -y create . --deps-only --packages=ocaml-base-compiler.5.1.0 && \
